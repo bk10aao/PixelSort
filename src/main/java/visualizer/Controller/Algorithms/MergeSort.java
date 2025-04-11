3d1586f35dcb32df942d455c34a157f124a9ce96
Dr-Psycho-Ben <kane_500@hotmail.com>
2025-04-11 16:03:30 +0100

    adding merge sort

--- src/main/java/visualizer/Controller/Algorithms/MergeSort.java
+++ src/main/java/visualizer/Controller/Algorithms/MergeSort.java
@@ -9,29 +9,23 @@ public class MergeSort {
 
     public static int[] aux;
 
-    public static List<List<Integer>> sort(int[] a, List<List<Integer>> results) {
+    public static void sort(int[] a, List<List<Integer>> results) {
         aux = new int[a.length];
         int low = 0;
         int high = a.length - 1;
         sort(aux, a, low, high, results);
-        return results;
     }
 
     private static void sort(int[] values1, int[] values2, int low, int high, List<List<Integer>> results) {
-        if (high <= low) return;
+        if (high <= low)
+            return;
         int[] merged = new int[values1.length + values2.length];
-        int index = 0;
-        for(int i = 0;  i < values1.length; i++) {
-            merged[index++] = values1[i];
-        }
-        for(int i = 0;  i < values2.length; i++) {
-            merged[index++] = values2[i];
-        }
+        System.arraycopy(values1, 0, merged, 0, values1.length);
+        System.arraycopy(values2, 0, merged, values1.length, values2.length);
         results.add(Arrays.stream(merged).boxed().toList());
-
-        int mid = low + (high - low)/2;
+        int mid = low + (high - low) / 2;
         sort(values1, values2, low, mid, results);
-        sort(values1,values2, mid+1, high, results);
+        sort(values1,values2, mid + 1, high, results);
         Commons.merge(values1, values2, low, mid, high);
     }
 }
