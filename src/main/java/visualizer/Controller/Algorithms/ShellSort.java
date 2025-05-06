91947b290aeb4ba01ac765a81f4519a923944436
Dr-Psycho-Ben <kane_500@hotmail.com>
2025-05-06 17:19:20 +0100

    fixing Shell Sort

--- src/main/java/visualizer/Controller/Algorithms/ShellSort.java
+++ src/main/java/visualizer/Controller/Algorithms/ShellSort.java
@@ -3,7 +3,6 @@ package visualizer.Controller.Algorithms;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
-import java.util.stream.Collectors;
 
 import static visualizer.Commons.Commons.exchange;
 import static visualizer.Commons.Commons.less;
@@ -12,19 +11,39 @@ public class ShellSort {
 
     public static List<List<Integer>> sort(int[] values) {
         List<List<Integer>> results = new ArrayList<>();
-        int n = values.length;
-        int h = 1;
         results.add(Arrays.stream(values).boxed().toList());
-        while (h < n / 3)
-            h = 3 * h + 1;
-        while (h >= 1) {
-            for(int i = h; i < n; i++)
-                for (int j = i; j >= h && less(values[j], values[j - h]); j -= h)
-                    exchange(values, j, j - h);
-            h /= 3;
-            results.add(Arrays.stream(values).boxed().toList());
-        }
-        results.add(Arrays.stream(values).boxed().collect(Collectors.toList()));
+        List<Integer> increments = generateIncrements(values.length);
+        for(int x = increments.size() - 1; x >= 0; x--)
+            hSort(values, increments.get(x), results);
         return results;
+    }
+
+    private static void hSort(int[] values, int h, List<List<Integer>> results) {
+        for(int x = h; x < values.length; x++)
+            for(int y = x; y >= h && less(values[y], values[y - h]); y -= h) {
+                exchange(values, y, y - h);
+                results.add(Arrays.stream(values).boxed().toList());
+            }
+    }
+
+    private static List<Integer> generateIncrements(int n) {
+        List<Integer> increments = new ArrayList<>();
+        int x = 0;
+        while (true) {
+            int y;
+            if (x % 2 == 0)
+                y = (int) (9 * (1L << x) - 9 * (1L << (x / 2)) + 1);
+            else
+                y = (int) (8 * (1L << x) - 6 * (1L << ((x + 1) / 2)) + 1);
+            if (y >= n)
+                break;
+            increments.add(y);
+            x++;
+        }
+        if (!increments.contains(1)) {
+            increments.add(1);
+            increments.sort(Integer::compareTo);
+        }
+        return increments;
     }
 }
