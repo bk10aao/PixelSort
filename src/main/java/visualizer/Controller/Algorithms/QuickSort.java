8dec97b9a5f934df3bd6682a5dee36f90e1b8957
Dr-Psycho-Ben <kane_500@hotmail.com>
2025-05-16 15:35:40 +0100

    updating exchange for QuickSort

--- src/main/java/visualizer/Controller/Algorithms/QuickSort.java
+++ src/main/java/visualizer/Controller/Algorithms/QuickSort.java
@@ -10,29 +10,29 @@ import static visualizer.Commons.Commons.less;
 import static visualizer.Commons.Commons.toList;
 
 public class QuickSort {
-
+   private static List<List<Integer>> results;
     public static List<List<Integer>> sort(int[] values) {
         if(values == null)
             throw new NullPointerException();
         if(values.length == 0)
             throw new IllegalArgumentException();
-        List<List<Integer>> results = new ArrayList<>();
-        shuffle(values, results);
-        sort(values, 0, values.length - 1, results);
+        results = new ArrayList<>();
+        shuffle(values);
+        sort(values, 0, values.length - 1);
         results.add(toList(values));
         return results;
     }
 
-    private static void sort(int[] values, int low, int high, List<List<Integer>> results) {
+    private static void sort(int[] values, int low, int high) {
         if(high <= low)
             return;
         results.add(Arrays.stream(values).boxed().toList());
-        int j = partition(values, low, high, results);
-        sort(values, low, j - 1, results);
-        sort(values, j + 1, high, results);
+        int j = partition(values, low, high);
+        sort(values, low, j - 1);
+        sort(values, j + 1, high);
     }
 
-    private static int partition(int[] values, int low, int high, List<List<Integer>> results) {
+    private static int partition(int[] values, int low, int high) {
         int leftPointer = low;
         int rightPointer = high + 1;
         while (true) {
@@ -44,13 +44,14 @@ public class QuickSort {
                     break;
             if(leftPointer >= rightPointer)
                 break;
-            exchange(values, leftPointer, rightPointer, results);
+            exchange(values, leftPointer, rightPointer);
+            results.add(toList(values));
         }
         exchange(values, low, rightPointer, results);
         return rightPointer;
     }
 
-    public static void shuffle(int[] values, List<List<Integer>> results) {
+    public static void shuffle(int[] values) {
         Random rand = new Random();
         for (int i = values.length - 1; i > 0; i--)
             exchange(values, i, rand.nextInt(i + 1), results);
