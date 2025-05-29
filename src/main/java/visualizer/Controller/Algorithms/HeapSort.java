1eaae33705cf0254c14469d6b62b715dc3af2f3b
Dr-Psycho-Ben <kane_500@hotmail.com>
2025-05-29 18:27:45 +0100

    cleanup

--- src/main/java/visualizer/Controller/Algorithms/HeapSort.java
+++ src/main/java/visualizer/Controller/Algorithms/HeapSort.java
@@ -11,39 +11,40 @@ import static visualizer.Commons.Commons.toList;
 
 public class HeapSort {
 
+    private static List<List<Integer>> results;
+
     public static List<List<Integer>> sort(int[] values) {
-        List<List<Integer>> results = initialize(values);
+        results = initialize(values);
         if(values.length == 1)
             return results;
         int[] heap = new int[values.length + 1];
         System.arraycopy(values, 0, heap, 1, values.length);
-        buildHeap(heap, values.length, results);
+        buildHeap(heap, values.length);
         results.add(toList(Arrays.copyOfRange(heap, 1, values.length)));
-        sortDown(heap, values.length, results);
+        sortDown(heap, values.length);
         return results;
     }
 
-    private static void buildHeap(int[] heap, int size, List<List<Integer>> results) {
+    private static void buildHeap(int[] heap, int size) {
         for (int i = size / 2; i >= 1; i--)
-            sink(heap, i, size, results);
+            sink(heap, i, size);
     }
 
-    private static void sortDown(int[] heap, int size, List<List<Integer>> results) {
+    private static void sortDown(int[] heap, int size) {
         while (size > 1) {
             exchange(heap, 1, size--);
-            sink(heap, 1, size, results);
+            sink(heap, 1, size);
             results.add(toList(Arrays.copyOfRange(heap, 1, heap.length)));
         }
     }
 
-    public static void sink(int[] binaryHeap, int index, int n, List<List<Integer>> results) {
+    public static void sink(int[] binaryHeap, int index, int n) {
         while (2 * index <= n) {
             int j = 2 * index;
             if (j < n && less(binaryHeap[j], binaryHeap[j + 1]))
                 j++;
-            if (!less(binaryHeap[index], binaryHeap[j])) {
+            if (!less(binaryHeap[index], binaryHeap[j]))
                 break;
-            }
             exchange(binaryHeap, index, j);
             results.add(Arrays.stream(binaryHeap).boxed().collect(Collectors.toList()));
             index = j;
